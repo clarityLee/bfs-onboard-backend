cb8b765052697d3c1312afda475cbb586a8358fe
Yunjing Lee <clarity.lee@gmail.com>
2021-10-08 19:10:58 -0400

    initial commit

---
+++ src/test/java/com/bfs/onboard/OnboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bfs.onboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
